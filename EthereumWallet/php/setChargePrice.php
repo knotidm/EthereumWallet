3b96e4cfa8bb460fd7e12b5d7ebe1ade7174b44c
Wojciech Sęk <knotidm@gmail.com>
2018-02-12 12:05:52 +0100

    admin get charge price

---
+++ EthereumWallet/php/setChargePrice.php
@@ -0,0 +1,24 @@
+<?php
+
+if (isset($_GET['chargePrice'])) $chargePrice = $_GET['chargePrice'];
+
+$host = "coinwallet.c26ysish9yud.eu-west-3.rds.amazonaws.com";
+$username = "coinwallet";
+$password = "coinwallet";
+$dbname = "coinwallet";
+
+$conn = new mysqli($host, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "UPDATE coinwallet.chargeprice SET chargeprice='$chargePrice' WHERE id='1'";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
